8c09776d65c3d3c71e72ec0bd7094f1d30b39a9e
HolyNekK <e2381093@metu.edu.tr>
2020-03-01 17:40:15 +0300

    Problem14 Solution

---
+++ Problem14.py
@@ -0,0 +1,10 @@
+def fact(number):
+    if number == 1:
+        return 1
+    return number*fact(number-1)
+
+
+a = int(input())
+for i in range(a):
+    n,m = list(map(int,raw_input().split()))
+    print(fact(n+m)//(fact(n)*fact(m))%(10**9+7))
